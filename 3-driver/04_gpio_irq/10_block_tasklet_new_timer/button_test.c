4714f4582ea0f241475bcf5450c3b0db0bdfbb6f
zm01 <1507951048@qq.com>
2023-11-13 03:34:22 +0800

    20231113

--- 3-driver/04_gpio_irq/10_block_tasklet_new_timer/button_test.c
+++ 3-driver/04_gpio_irq/10_block_tasklet_new_timer/button_test.c
@@ -65,3 +65,65 @@ int main(int argc, char **argv)
 }
 
 
+/*
+$ insmod gpio_key_drv.ko 
+
+[ 1011.014278] /home/book/learn_c/3-driver/04_gpio_irq/09_block_timer_tasklet/gpio_key_drv.c gpio_key_init line 271
+[ 1011.035530] /home/book/learn_c/3-driver/04_gpio_irq/09_block_timer_tasklet/gpio_key_drv.c gpio_key_probe line 178
+[ 1011.054440] key_timer_expire key 110 1
+[ 1011.058241] key_timer_expire key 129 1
+$ ./button_test /dev/100ask_gpio_key
+get button: 0x6e01
+get button: 0x8101
+get button: -1
+get button: -1
+get button: -1
+get button: -1
+get button: -1
+get button: -1
+get button: -1
+get button: -1
+[ 1022.924947] key_tasklet_func key 110 0
+[ 1022.954417] key_timer_expire key 110 0
+get button: 0x6e00
+[ 1023.131004] key_tasklet_func key 110 1
+[ 1023.154588] key_timer_expire key 110 1
+get button: 0x6e01
+[ 1024.237889] key_tasklet_func key 110 0
+[ 1024.264466] key_timer_expire key 110 0
+get button: 0x6e00
+[ 1024.493994] key_tasklet_func key 110 1
+[ 1024.514434] key_timer_expire key 110 1
+get button: 0x6e01
+[ 1025.241946] key_tasklet_func key 110 0
+[ 1025.264427] key_timer_expire key 110 0
+get button: 0x6e00
+[ 1025.477057] key_tasklet_func key 110 1
+[ 1025.504365] key_timer_expire key 110 1
+get button: 0x6e01
+[ 1026.126915] key_tasklet_func key 110 0
+[ 1026.154438] key_timer_expire key 110 0
+get button: 0x6e00
+[ 1027.020591] key_tasklet_func key 110 1
+[ 1027.044425] key_timer_expire key 110 1
+get button: 0x6e01
+[ 1030.030081] key_tasklet_func key 129 0
+[ 1030.054469] key_timer_expire key 129 0
+get button: 0x8100
+[ 1030.261987] key_tasklet_func key 129 1
+[ 1030.284439] key_timer_expire key 129 1
+get button: 0x8101
+[ 1030.851004] key_tasklet_func key 129 0
+[ 1030.874455] key_timer_expire key 129 0
+get button: 0x8100
+[ 1031.097589] key_tasklet_func key 129 1
+[ 1031.124426] key_timer_expire key 129 1
+get button: 0x8101
+[ 1031.887572] key_tasklet_func key 129 0
+[ 1031.914415] key_timer_expire key 129 0
+get button: 0x8100
+[ 1032.290525] key_tasklet_func key 129 1
+[ 1032.314426] key_timer_expire key 129 1
+get button: 0x8101
+
+*/
